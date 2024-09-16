77f15b91214a67f1f4879cca4aca280c7c2b30ea
Dare Samuel <dawakesam@gmail.com>
2024-09-16 15:17:59 +0100

    Nav Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import Nav from "./components/Nav";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from "./sections";
+
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="py-10 padding-x">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="w-full py-16 padding-x sm:py-32">
+        <Subscribe />
+      </section>
+      <section className="pb-8 bg-black padding-x padding-t">
+        <Footer />
+      </section>
+    </main>
+  );
 }
 
 export default App;
